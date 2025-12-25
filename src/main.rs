be58d1d6362afb040aa08c18ae04748c2acbf7e4
benmandrew <benmandrew@gmail.com>
2025-12-25 20:01:56 +0000

    feat: Split terminal into two panes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use color_eyre::Result;
 use crossterm::event::{self, Event};
+use ratatui::prelude::*;
+use ratatui::widgets::{Block, BorderType, Borders, Paragraph};
 use ratatui::{DefaultTerminal, Frame};
 
 fn main() -> Result<()> {
@@ -20,5 +22,24 @@ fn run(mut terminal: DefaultTerminal) -> Result<()> {
 }
 
 fn render(frame: &mut Frame) {
-    frame.render_widget("hello world", frame.area());
+    let layout = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(vec![Constraint::Percentage(50), Constraint::Percentage(50)])
+        .split(frame.area());
+    frame.render_widget(
+        Paragraph::new("Left").block(
+            Block::new()
+                .border_type(BorderType::Rounded)
+                .borders(Borders::ALL),
+        ),
+        layout[0],
+    );
+    frame.render_widget(
+        Paragraph::new("Right").block(
+            Block::new()
+                .border_type(BorderType::Rounded)
+                .borders(Borders::ALL),
+        ),
+        layout[1],
+    );
 }
